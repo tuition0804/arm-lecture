8b6c71606c929bfd006ce62a92a9859f9f77269d
yixiang804 <tuition0804@hotmail.com>
2015-03-26 20:49:48 +0800

    try to upload my file

--- lab-1/fib.s
+++ lab-1/fib.s
@@ -27,7 +27,19 @@ fibonacci:
 	@ Recursive call to fibonacci with R4 - 2 as parameter
 
 	@ R0 = R5 + R0 (update flags)
+        subs r4, r0, #0        ;@ r4 = r0 - 0
+        ble .L3        ;@ if (r0 <= 0) goto .L3
 
+        cmp r4, #1        ;@ Compare r4 to 1
+        beq .L4        ;@ if (r4 == 1) goto .L4
+
+        add r0, r4, #4294967295  ;@ r0 = r4 + 4294967295 (or #0xFFFFFFFF)
+        bl fibonacci          ;@ goto fibonacci @PC relative address
+
+        mov r5, r0        ;@ r5 = r0
+        sub r0, r4, #2    ;@ r0 = r4 - 2
+        bl fibonacci;
+        adds r0,r5,r0;
 	pop {r3, r4, r5, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
--- lab-1/fibseq.c
+++ lab-1/fibseq.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-
+#include <time.h>
 extern int fibonacci(int x);
-
 int main(int argc, char **argv)
 {
-  int number=0;
-  int result=0;
-
-  scanf("%d",&number);
-  result = fibonacci(number);   
-  printf("The fibonacci sequence at %d is: %d\n", number, result);
+int number=0;
+int result=0;
+clock_t t1,t2;
+scanf("%d",&number);
+t1=clock();
+result = fibonacci(number);
+t2=clock();
+printf("The fibonacci sequence at %d is: %d\nIt cost %lf seconds and %d cycles.\n\n", number, result,(t2-t1)/(double)(CLOCKS_PER_SEC),(int)(t2-t1));
 }
-
